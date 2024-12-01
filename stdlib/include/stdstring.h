13ec115bb0ad8657fcc352d9349d967961120a02
Jamie Connelly <itsphireyt@gmail.com>
2024-12-01 18:28:34 +0000

    Add strcmp

--- stdlib/include/stdstring.h
+++ stdlib/include/stdstring.h
@@ -3,5 +3,17 @@
  * Basic Operating System, String Utilities
  */
 
+/**
+ * Get the length of a string
+ * @param str The string to check the length of
+ * @return The length of the given string
+ */
 int strlen(const char* str);
-int strcmp(const char* str_a, const char* str_b);
+
+/**
+ * Compare two strings lexicographically
+ * @param s1 The first string
+ * @param s2 The second string
+ * @return s1 < s2 = -1, s1 == s2 = 0, s1 > s2 = 1
+ */
+int strcmp(const char* s1, const char* s2);
--- stdlib/stdstring.c
+++ stdlib/stdstring.c
@@ -5,6 +5,11 @@
 
 #include "include/stdstring.h"
 
+/**
+ * Get the length of a string
+ * @param str The string to check the length of
+ * @return The length of the given string
+ */
 int strlen(const char* str)
 {
     int length = 0;
@@ -18,7 +23,19 @@ int strlen(const char* str)
     return length;
 }
 
-int strcmp(const char* str_a, const char* str_b)
+/**
+ * Compare two strings lexicographically
+ * @param s1 The first string
+ * @param s2 The second string
+ * @return s1 < s2 = -1, s1 == s2 = 0, s1 > s2 = 1
+ */
+int strcmp(const char* s1, const char* s2)
 {
-    return 0;
+    while (*s1 != '\0' && (*s1 == *s2))
+    {
+        ++s1;
+        ++s2;
+    }
+
+    return *s1 - *s2;
 }
